21852f9549cde4a83795aeeafe456c286b304fcb
leonardo <leochp@gmail.com>
2016-04-27 21:24:23 -0300

    views

--- desabono/urls.py
+++ desabono/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from posto import views
 
 urlpatterns = [
     # Examples:
-    # url(r'^$', 'desabono.views.home', name='home'),
+    url(r'^$', 'posto.views.index', name='index'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
--- posto/models.py
+++ posto/models.py
@@ -4,12 +4,47 @@ from django.db import models
 
 
 class credor(models.Model):
-    nome = models.CharField(max_length=2000)
-    cnpj = models.CharField(max_length=12)
-    endereco = models.CharField(max_length=8000, blank=True)
-    email = models.EmailField()
-    tipo = models.CharField(max_length=255)
-    responsavel = models.CharField(max_length=255)
-    ip_user = models.CharField(max_length=255)
-    descricao = models.CharField(max_length=8000)
+    nome = models.CharField(max_length=2000,
+                            null=True)
+    cnpj = models.CharField(max_length=12,
+                            null=True)
+    endereco = models.CharField(max_length=8000,
+                                blank=True,
+                                null=True)
+    email = models.EmailField(null=True)
+    tipo = models.CharField(max_length=255,
+                            null=True)
+    ip_user = models.CharField(max_length=255,
+                               null=True)
+    data_add = models.DateTimeField(null=True)
 
+
+class divida(models.Model):
+    credor_cnpj = models.ForeignKey(credor,
+                                    on_delete=models.CASCADE)
+    data_add = models.DateTimeField(null=True)
+    valor = models.FloatField(null=True)
+    vencimento = models.CharField(max_length=20,
+                                  null=True)
+    descricao = models.CharField(max_length=8000,
+                                 null=True)
+    citado = models.CharField(max_length=8000,
+                              null=True)
+
+    DUPLICIDATE = 'DV'
+    CHEQUE = 'CD'
+    SALDO = 'SD'
+    JUROS = 'JP'
+    OUTRO = 'OT'
+    tipo_choices = (
+        (DUPLICIDATE, 'Duplicidade'),
+        (CHEQUE, 'Cheque devolvido'),
+        (SALDO, 'Saldo devedor'),
+        (JUROS, 'Juros nao pagos'),
+        (OUTRO, 'Outro'),
+    )
+    tipo_divida = models.CharField(max_length=2,
+                                   null=True,
+                                   choices=tipo_choices,
+                                   default=OUTRO)
+    termos = models.BooleanField(default=False)
