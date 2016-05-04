6355b6662bc5e5afcd77fa8df3ab23306fc7d3f8
leonardo <leochp@gmail.com>
2016-05-03 21:36:49 -0300

    cadastro de dívida

--- desabono/urls.py
+++ desabono/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 from posto import views as posto_views
+from divida import views as divida_views
 
 urlpatterns = [
     url(r'^$', posto_views.index, name='index'),
     url(r'^cadastro/', posto_views.cadastro, name='cadastro'),
     url(r'^usuarios/', posto_views.credores, name='credores'),
+    url(r'^divida/', divida_views.divida, name='divida'),
     url(r'^admin/', include(admin.site.urls)),
 ]
--- divida/views.py
+++ divida/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from .forms import form_divida
+from django.http import HttpResponseRedirect
+from datetime import datetime
+from .models import divida
 
-# Create your views here.
+
+def divida(request):
+    if request.method == 'POST':
+        form = form_divida(request.POST)
+        if form.is_valid():
+            new_divida = form.save(commit=False)
+            new_divida.data_add = datetime.now()
+            new_divida.save()
+            return HttpResponseRedirect('/')
+    else:
+        form = form_divida()
+    return render(request, 'divida/divida.html', {'form': form})
