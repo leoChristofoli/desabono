1031b2a5c45fc7724c1fe76c3fd45ded41229b66
a487czz <a487czz@mmm.com>
2016-05-06 10:36:32 -0300

    fix models divida

--- posto/models.py
+++ posto/models.py
@@ -1,3 +1,4 @@
+# coding=utf-8
 from __future__ import unicode_literals
 
 from django.db import models
@@ -5,21 +6,39 @@ from django.contrib.auth.models import User
 
 
 class credor(models.Model):
-    nome = models.CharField(max_length=2000,
-                            null=True)
-    sobrenome = models.CharField(max_length=2000,
-                                 null=True)
-    cnpj = models.CharField(max_length=12,
-                            null=True,
-                            unique=True)
-    endereco = models.CharField(max_length=8000,
-                                blank=True,
-                                null=True)
-    email = models.ForeignKey(User,
-                              null=True,
-                              on_delete=models.CASCADE)
-    tipo = models.CharField(max_length=255,
-                            null=True)
-    ip_user = models.CharField(max_length=255,
-                               null=True)
-    data_add = models.DateTimeField(null=True)
+    nome = models.CharField(
+        max_length=2000,
+        null=True
+    )
+    sobrenome = models.CharField(
+        max_length=2000,
+        null=True
+    )
+    cnpj = models.CharField(
+        max_length=12,
+        null=True,
+        unique=True
+    )
+    endereco = models.CharField(
+        max_length=8000,
+        blank=True,
+        null=True,
+        verbose_name='Endereço'
+    )
+    email = models.ForeignKey(
+        User,
+        null=True,
+        on_delete=models.CASCADE
+    )
+    tipo = models.CharField(
+        max_length=255,
+        null=True,
+        verbose_name='Tipo da dívida'
+    )
+    ip_user = models.CharField(
+        max_length=255,
+        null=True
+    )
+    data_add = models.DateTimeField(
+        null=True
+    )
