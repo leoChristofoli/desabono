5d5b8502b602cfc001a47d50648e20fe79adf186
a487czz <a487czz@mmm.com>
2016-07-08 09:08:00 -0300

    debug false

--- desabono/settings.py
+++ desabono/settings.py
@@ -24,7 +24,7 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 SECRET_KEY = "3o=&mpr9w6l^bzujj67u64^sfj%i$r1e%%j=-rhhr@mry@m81e"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 # Application definition
 
